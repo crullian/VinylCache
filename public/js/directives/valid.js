f846c8c7027371688ea928f331ad81752e3ea660
crullian <crullian@gmail.com>
2015-07-10 20:04:04 -0400

    Custom validation

---
+++ public/js/directives/valid.js
@@ -0,0 +1,10 @@
+app.directive('valid', function() {
+  return {
+    restrict: 'A',
+    templateUrl: 'js/directives/valid/valid.html',
+    scope: true,
+    link: function(scope, element, attrs) {
+
+    }
+  };
+});

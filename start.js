4243f4eeb0cb327e60e5557073296e4c339ea59b
crullian <crullian@gmail.com>
2016-01-24 11:19:03 -0500

    Fix hot loading in dev

--- start.js
+++ start.js
@@ -27,7 +27,11 @@ if (isDev) {
      hot: true,
      stats: {colors: true},
      historyApiFallback: true,
-     inline: true
+     inline: true,
+     proxy: {
+      '*': 'http://localhost:1335'
+     }
+
   }).listen(8080, 'localhost', function (err, result) {
      if (err) {
        console.log(err);

801de220157c6a61e8d853f56c1d8df43f096ea1
crullian <crullian@gmail.com>
2017-06-03 20:13:33 -0400

    Update index files and package json to add MUI.

--- public/js/index.js
+++ public/js/index.js
@@ -7,10 +7,24 @@ import createLogger from 'redux-logger'
 import rootReducer from './reducers'
 import RecordApp from './container/RecordApp.js'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import {deepPurple400} from 'material-ui/styles/colors'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
+
 const middleware = [ thunk ]
 if (process.env.NODE_ENV !== 'production') {
   middleware.push(createLogger())
 }
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: deepPurple400
+  }
+});
 
 const store = createStore(
   rootReducer,
@@ -19,7 +33,9 @@ const store = createStore(
 
 render(
   <Provider store={store}>
-    <RecordApp />
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <RecordApp />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('content')
 )
